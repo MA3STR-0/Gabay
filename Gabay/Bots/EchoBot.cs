0be5370b210148d1ca8a5aa7f7d1c24bc42422d5
Matthew Zapra <matthewzapra0@gmail.com>
2024-09-03 04:35:48 +0800

    Added adaptive card for user input

--- Gabay/Bots/EchoBot.cs
+++ Gabay/Bots/EchoBot.cs
@@ -5,6 +5,8 @@ using Microsoft.Bot.Schema;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Gabay.Cards;
+using Newtonsoft.Json;
 
 namespace Gabay.Bots
 {
@@ -18,14 +20,66 @@ namespace Gabay.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            /*
+            LoginCard Card = new LoginCard();
+            Attachment loginAttachment = LoginCard.cardSchema();
+            var welcomeText = "Hi, I am Gabay an online chatbot interface todo list tool. To continue please input your username and password";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(loginAttachment), cancellationToken);
                 }
             }
+            */
+
+            string cardJson = @"
+            {
+                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+                ""type"": ""AdaptiveCard"",
+                ""version"": ""1.3"",
+                ""body"": [
+                    {
+                        ""type"": ""Container"",
+                        ""items"": [
+                            {
+                                ""type"": ""Input.Text"",
+                                ""placeholder"": ""Placeholder text"",
+                                ""label"": ""Username:"",
+                                ""id"": ""username""
+                            },
+                            {
+                                ""type"": ""Input.Text"",
+                                ""placeholder"": ""Placeholder text"",
+                                ""label"": ""Password"",
+                                ""id"": ""password"",
+                                ""isMultiline"": false,
+                                ""style"": ""text""
+                            }
+                        ]
+                    }
+                ],
+                ""actions"": [
+                    {
+                        ""type"": ""Action.Submit"",
+                        ""title"": ""Submit"",
+                        ""data"": {
+                            ""action"": ""submitForm""
+                        }
+                    }
+                ]
+            }";
+
+
+            // Convert the card JSON string to an Attachment
+            var adaptiveCard = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(cardJson),
+            };
+
+            // Send the card to the user
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(adaptiveCard), cancellationToken);
         }
     }
 }
---
+++ Gabay/Cards/LoginCard.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Gabay.Cards
+{
+    public class LoginCard
+    {
+       private string username { get; set; }
+       private string password { get; set; }
+
+        public static Attachment cardSchema() {
+            var cardJson = @"
+        {
+            ""type"": ""AdaptiveCard"",
+            ""body"": [
+                {
+                    ""type"": ""TextBlock"",
+                    ""text"": ""Login"",
+                    ""weight"": ""Bolder"",
+                    ""size"": ""Medium""
+                },
+                {
+                    ""type"": ""Input.Text"",
+                    ""id"": ""username"",
+                    ""placeholder"": ""Enter your username"",
+                    ""style"": ""text""
+                },
+                {
+                    ""type"": ""Input.Text"",
+                    ""id"": ""password"",
+                    ""placeholder"": ""Enter your password"",
+                    ""style"": ""password"",
+                    ""isRequired"": true,
+                    ""errorMessage"": ""Password is required.""
+                }
+            ],
+            ""actions"": [
+                {
+                    ""type"": ""Action.Submit"",
+                    ""title"": ""Submit""
+                }
+            ],
+            ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+            ""version"": ""1.3""
+        }";
+
+            // Convert the card JSON string to an Attachment
+            var adaptiveCard = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(cardJson),
+            };
+
+            return adaptiveCard;
+        }
+    }
+}
